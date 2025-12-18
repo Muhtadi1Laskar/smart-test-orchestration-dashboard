7558e28e65d0f0d02118dd72c9ef9adb3acc9a82
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-12-19 03:48:11 +0600

    Added the error handler

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import helment from 'helmet';
+import morgan from 'morgan';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(helment());
+app.use(morgan("dev"));
+
+app.use("/api", router);
+
+app.use(errorHandler);
+
+export default app;
